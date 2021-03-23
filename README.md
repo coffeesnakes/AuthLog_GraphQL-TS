4305385a3d2cacd1e495e50e34ef483a69d6d7f0
Jacob Berg <coffeesnakes@gmail.com>
2021-03-22 18:05:05 -0700

    email validation and user registry, Redis implementation to store session data in process

--- README.md
+++ README.md
@@ -8,7 +8,43 @@ Todo:
 -confirmation emails when users register
 -forgot password and changing password functionality
 
-Remember to:
--test
--test
--test
+
+Technologies: GraphQL, Postgres, Apollo, bcrypt, redis
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+notes on graphQL:
+GraphQL seems to be much more capable than a REST API, you can set each field to have it's own query parameters and nested objects can even have their own arguments.
+A cool feature of GraphQL is that you can pass arguments into scalar fields, so if a client wanted to have the price of a painting returned, we could return something like the following:
+{
+	painting(id: "3005") {
+		name
+		price(unit: EUROS)
+	}
+}
+
+And the thing I want to highlight is in the price, we're specifying we wanted that data to be outputted in EUROS, and we could have that unit change depending on location, or a set preference whatever really. Just a cool functionality of GraphQL.
+
+
+notes on Redis use:
+Redis is storing session data, I have cookies holding properties that only allow access by HTTP so javascript can't access it and secure only works for HTTPS when in production.
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { ApolloServer } from "apollo-server-express";
 import "reflect-metadata";
-import * as Express from "express";
+import Express from "express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
 import { RegisterResolver } from "./modules/user/Register";
-
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { redis } from "./redis";
+import cors from "cors";
 
 const main = async () => {
 	await createConnection();
@@ -12,15 +15,42 @@ const main = async () => {
 		resolvers: [RegisterResolver],
 	});
 
-	const apolloServer = new ApolloServer({schema})
+	const apolloServer = new ApolloServer({
+		schema,
+		context: ({ req }: any) => ({ req })
+	});
 
 	const app = Express();
+
+	const RedisStore = connectRedis(session);
+
+	app.use(cors({
+		credentials: true,
+		origin: "http://localhost:3005"
+	}));
+
+	app.use(
+		session({
+			store: new RedisStore({
+				client: redis as any
+			}),
+			name: "qid",
+			secret: "SESSION_SECRET",
+			resave: false,
+			saveUninitialized: false,
+			cookie: {
+				httpOnly: true,
+				secure: process.env.NODE_ENV === "production",
+				maxAge: 1000 * 60 * 60 * 24 * 7 * 365 // this is equal to 7 years
+			},
+		})
+	);
 
 	apolloServer.applyMiddleware({ app });
 
 	app.listen(3005, () => {
 		console.log("server started on http://localhost:3005/graphql")
 	})
-}
+};
 
 main();
